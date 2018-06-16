83bb6fd1d57061f20fd05b57b99825ed89dcefd0
seunghun <seunghun.chan@gmail.com>
2018-06-16 12:12:26 +0900

    added new js for typography

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import styles from './style.scss'
 
-export default () => <div className={styles.title}>Lalalala</div>;
+export default () => (
+  <div className={styles.title}>
+    <h1>Hello</h1>
+    <p>lalala lalala</p>
+  </div>
+);
